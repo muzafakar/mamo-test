668d38eaa4d7d0bee5e1bb8c7a8b360b4c4fd50c
muzafakar <muhammad.zulfakar@students.amikom.ac.id>
2021-07-16 03:43:58 +0800

    add condition for last-dot expression

--- app/src/main/java/com/mhmdzlfkr/mamotest/widget/CurrencyUtil.kt
+++ app/src/main/java/com/mhmdzlfkr/mamotest/widget/CurrencyUtil.kt
@@ -1,9 +1,23 @@
 package com.mhmdzlfkr.mamotest.widget
 
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.*
+
 class CurrencyUtil {
-    companion object{
+    companion object {
         private const val regexPattern = "([0-9]+\\.[0-9]{0,2})+"
+        private const val decimalPattern = "###,###,###"
     }
 
-    fun isDecimalStringValid(expression: String) = Regex(regexPattern).matches(expression)
+    fun isDecimalStringValid(expression: String): Boolean {
+        val dotCount = expression.count { it.toString() == AppConst.DOT }
+        return Regex(regexPattern).matches(expression) && dotCount == 1
+    }
+
+    fun formatAmount(amountStr: String): String {
+        val amount = amountStr.toDoubleOrNull() ?: return ""
+        val formatter = DecimalFormat(decimalPattern, DecimalFormatSymbols.getInstance(Locale.getDefault()))
+        return formatter.format(amount)
+    }
 }
--- app/src/test/java/com/mhmdzlfkr/mamotest/widget/CurrencyUtilTest.kt
+++ app/src/test/java/com/mhmdzlfkr/mamotest/widget/CurrencyUtilTest.kt
@@ -1,7 +1,7 @@
 package com.mhmdzlfkr.mamotest.widget
 
 import org.hamcrest.core.Is.`is`
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
@@ -14,87 +14,136 @@ class CurrencyUtilTest {
         currencyUtil = CurrencyUtil()
     }
 
+    // isDecimalStringValid()
+
     @Test
     fun isDecimalStringValid_halfDecimalAmount_returnsValid() {
         val input = "10000.1"
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(true))
+        assertThat(result, `is`(true))
     }
 
     @Test
     fun isDecimalStringValid_noDecimalAmount_returnsValid() {
         val input = "10000."
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(true))
+        assertThat(result, `is`(true))
     }
 
     @Test
     fun isDecimalStringValid_fullDecimalAmount_returnsValid() {
         val input = "10000.12"
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(true))
+        assertThat(result, `is`(true))
     }
 
     @Test
     fun isDecimalStringValid_excessiveDecimalAmount_returnsInvalid() {
         val input = "10000.123"
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(false))
+        assertThat(result, `is`(false))
     }
 
     @Test
     fun isDecimalStringValid_excessiveComma_returnsInvalid() {
         val input = "10000..123"
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(false))
+        assertThat(result, `is`(false))
     }
 
-   @Test
+    @Test
     fun isDecimalStringValid_excessiveCommaEnd_returnsInvalid() {
-        val input = "10000.123."
+        val input = "10000.12344."
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(false))
+        assertThat(result, `is`(false))
     }
 
     @Test
     fun isDecimalStringValid_commaLeadingAmountAndExcessiveDecimal_returnsInvalid() {
         val input = ".123"
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(false))
+        assertThat(result, `is`(false))
     }
 
     @Test
     fun isDecimalStringValid_commaLeadingAmount_returnsInvalid() {
         val input = ".12"
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(false))
+        assertThat(result, `is`(false))
     }
 
     @Test
     fun isDecimalStringValid_zeroLeadingAmount_returnsValid() {
         val input = "0.12"
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(true))
+        assertThat(result, `is`(true))
     }
 
- @Test
+    @Test
     fun isDecimalStringValid_zeroLeadingAmountAndExcessiveDecimal_returnsInvalid() {
         val input = "0.123"
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(false))
+        assertThat(result, `is`(false))
     }
 
     @Test
     fun isDecimalStringValid_normalAmount_returnsInvalid() {
         val input = "10000"
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(false))
+        assertThat(result, `is`(false))
     }
 
     @Test
     fun isDecimalStringValid_emptyAmount_returnsInvalid() {
         val input = ""
         val result = currencyUtil.isDecimalStringValid(input)
-        Assert.assertThat(result, `is`(false))
+        assertThat(result, `is`(false))
+    }
+
+    @Test
+    fun formatAmount_emptyAmount_returnValidFormat() {
+        val input = ""
+        val expected = ""
+        val result = currencyUtil.formatAmount(input)
+        assertThat(result, `is`(expected))
+    }
+    @Test
+    fun formatAmount_singleAmount_returnValidFormat() {
+        val input = "1"
+        val expected = "1"
+        val result = currencyUtil.formatAmount(input)
+        assertThat(result, `is`(expected))
+    }
+
+    @Test
+    fun formatAmount_dozensAmount_returnValidFormat() {
+        val input = "12"
+        val expected = "12"
+        val result = currencyUtil.formatAmount(input)
+        assertThat(result, `is`(expected))
+    }
+
+    @Test
+    fun formatAmount_hundredsAmount_returnValidFormat() {
+        val input = "123"
+        val expected = "123"
+        val result = currencyUtil.formatAmount(input)
+        assertThat(result, `is`(expected))
+    }
+
+    @Test
+    fun formatAmount_thousandsAmount_returnValidFormat() {
+        val input = "1234"
+        val expected = "1,234"
+        val result = currencyUtil.formatAmount(input)
+        assertThat(result, `is`(expected))
+    }
+
+    @Test
+    fun formatAmount_thousandsAndDozensAmount_returnValidFormat() {
+        val input = "12345"
+        val expected = "12,345"
+        val result = currencyUtil.formatAmount(input)
+        assertThat(result, `is`(expected))
     }
 }
