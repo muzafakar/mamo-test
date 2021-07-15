53f91123af2977e831ef516fa85ef43f303a3e97
muzafakar <muhammad.zulfakar@students.amikom.ac.id>
2021-07-15 23:32:26 +0800

    init

---
+++ app/src/main/java/com/mhmdzlfkr/mamotest/widget/CurrencyWidget.kt
@@ -0,0 +1,11 @@
+package com.mhmdzlfkr.mamotest.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class CurrencyWidget @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+}
